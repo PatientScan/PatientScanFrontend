db15fe3eebb0b472afd4b07b996997073227bd9d
Reimnop <rayngu2005@gmail.com>
2026-03-21 23:35:27 +0700

    Get rid of ideal crap

--- src/lib/QrScanner.svelte
+++ src/lib/QrScanner.svelte
@@ -18,7 +18,7 @@
 		lastScanned = null;
 		try {
 			stream = await navigator.mediaDevices.getUserMedia({
-				video: { facingMode: 'environment', width: { ideal: 720 }, height: { ideal: 1280 } }
+				video: { facingMode: 'environment' }
 			});
 			videoEl.srcObject = stream;
 			await videoEl.play();
